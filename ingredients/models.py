16c88699cb76c9f32a8c227b716bb8e741711767
nine57 <gusrn015@gmail.com>
2022-04-06 12:14:17 +0900

    ADD: ingredients_initial_modeling

--- ingredients/models.py
+++ ingredients/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+
+class Bread(models.Model):
+    name = models.CharField(max_length=100)
+    in_stock = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+
+    class Meta:
+        db_table = "breads"
+
+
+class Topping(models.Model):
+    name = models.CharField(max_length=100)
+    in_stock = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+
+    class Meta:
+        db_table = "toppings"
+
+
+class Cheese(models.Model):
+    name = models.CharField(max_length=100)
+    in_stock = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+
+    class Meta:
+        db_table = "cheeses"
+
+
+class Sauce(models.Model):
+    name = models.CharField(max_length=100)
+    in_stock = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+
+    class Meta:
+        db_table = "Sauces"
